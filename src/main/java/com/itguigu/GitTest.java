f28a9238470725901f1822290ac9223d43bf59d7
xiaokang1206 <chenkang1206@163.com>
2022-08-03 16:10:27 +0800

    hot-fix commit2

--- src/main/java/com/itguigu/GitTest.java
+++ src/main/java/com/itguigu/GitTest.java
@@ -5,8 +5,7 @@ public class GitTest {
     public static void main(String[] args) {
 
         System.out.println("hello,world");
-
-        System.out.println("hello,world33");
+        System.out.println("hot-fix test");
 
 
     }
