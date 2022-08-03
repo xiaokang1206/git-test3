87ed1c42b4d93312c30aea7dece717a7639c45ec
xiaokang1206 <chenkang1206@163.com>
2022-08-03 16:18:11 +0800

    hot-fix commit3

--- src/main/java/com/itguigu/GitTest.java
+++ src/main/java/com/itguigu/GitTest.java
@@ -4,8 +4,10 @@ public class GitTest {
 
     public static void main(String[] args) {
 
-        System.out.println("hello,world");
-        System.out.println("hot-fix test");
+        System.out.println("hello,world1");
+        System.out.println("hello,world2");
+        System.out.println("hello,world3");
+        System.out.println("hot-fix-merge");
 
 
     }
