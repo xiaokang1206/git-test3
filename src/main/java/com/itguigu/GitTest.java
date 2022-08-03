7661f57ff0b2e9765447fa15bd766cc56a7314da
xiaokang1206 <chenkang1206@163.com>
2022-08-03 16:17:31 +0800

    master commit3

--- src/main/java/com/itguigu/GitTest.java
+++ src/main/java/com/itguigu/GitTest.java
@@ -4,11 +4,12 @@ public class GitTest {
 
     public static void main(String[] args) {
 
-        System.out.println("hello,world");
+        System.out.println("hello,world1");
+        System.out.println("hello,world2");
+        System.out.println("hello,world3");
+        System.out.println("master-merge");
 
-        System.out.println("master test");
-        System.out.println("hello,world33");
-        System.out.println("hot-fix test");
+
 
 
     }
