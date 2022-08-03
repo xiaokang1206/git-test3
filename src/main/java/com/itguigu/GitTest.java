ead2ed76a55b9fd3f322ddab377e46e74abbc475
xiaokang1206 <chenkang1206@163.com>
2022-08-03 16:11:19 +0800

    master commit2

--- src/main/java/com/itguigu/GitTest.java
+++ src/main/java/com/itguigu/GitTest.java
@@ -5,7 +5,7 @@ public class GitTest {
     public static void main(String[] args) {
 
         System.out.println("hello,world");
-
+        System.out.println("master test");
         System.out.println("hello,world33");
 
 
