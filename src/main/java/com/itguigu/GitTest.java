89a8ebb73ba644c5322e104fa9859bb87eddbd79
xiaokang1206 <104833778+xiaokang1206@users.noreply.github.com>
2022-08-03 16:55:06 +0800

    Update GitTest.java

--- src/main/java/com/itguigu/GitTest.java
+++ src/main/java/com/itguigu/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello,world3");
         System.out.println("hello,world4");
         System.out.println("push test!");
+        System.out.println("pull test!");
     }
 }
