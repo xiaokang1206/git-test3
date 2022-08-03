c802baa974b56f971c3c8e0e9b3e405636e14b8f
xiaokang1206 <chenkang1206@163.com>
2022-08-03 16:46:58 +0800

    push commit

--- src/main/java/com/itguigu/GitTest.java
+++ src/main/java/com/itguigu/GitTest.java
@@ -7,7 +7,7 @@ public class GitTest {
         System.out.println("hello,world1");
         System.out.println("hello,world2");
         System.out.println("hello,world3");
-
         System.out.println("hello,world4");
+        System.out.println("push test!");
     }
 }
